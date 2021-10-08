23bc85ee072f71e22b0894dc4576ac4dbb42ebd6
ahnafabir <ahnafabir313@gmail.com>
2021-10-08 17:48:19 +0600

    added masonry layout on activity page

--- src/pages/Activity/Activity.js
+++ src/pages/Activity/Activity.js
@@ -1,29 +1,40 @@
 import './Activity.css';
 import React, { useContext } from 'react';
-import { Container } from 'react-bootstrap'
-import PostCard from '../../components/PostCard/PostCard';
 import { PostContext } from '../../App';
+import Masonry from 'react-masonry-css';
+import { Container } from 'react-bootstrap';
+import PostCard from '../../components/PostCard/PostCard';
 import SkeletonCard from '../../components/SkeletonCard/SkeletonCard';
 
 const Activity = ({ dataLoaded }) => {
   const [allPosts, setAllPosts] = useContext(PostContext)
 
+  const breakpoints = {
+    default: 3,
+    1010: 2,
+    575: 1
+  }
+
   return (
     <Container>
       <h4 className="page_title">Activities</h4>
 
-      <div className="post_container">
-          {
-            allPosts.map(post =>
-              <PostCard
-                key={post._id}
-                post={post}
-                setAllPosts={setAllPosts}
-              />
-            )
-          }
-      </div>
-      
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {
+          allPosts.map(post =>
+            <PostCard
+              key={post._id}
+              post={post}
+              setAllPosts={setAllPosts}
+            />
+          )
+        }
+      </Masonry>
+
       {
         !dataLoaded &&
         <div className="post_container">
